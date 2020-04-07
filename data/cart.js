944603ecb3a115adabed8689ca265d7c1ecb564c
internetcharles <charlesodellsmith@gmail.com>
2020-04-07 15:19:03 -0700

    Added cart index and cart css

---
+++ data/cart.js
@@ -0,0 +1,15 @@
+const cart = [{
+    id: 'yeezy',
+    quantity: 5
+}, {
+    id: 'fieriChucks',
+    quantity: 8
+}, {
+    id: 'oldBoot',
+    quantity: 2
+}, {
+    id: 'tissueBox',
+    quantity: 3
+}];
+
+export default cart;
