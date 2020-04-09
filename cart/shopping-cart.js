0ad4a71d1fb5a76d1be2ec0a30fad3afe53fc7ad
internetcharles <charlesodellsmith@gmail.com>
2020-04-09 09:45:38 -0700

    CART WORKS

--- cart/shopping-cart.js
+++ cart/shopping-cart.js
@@ -1,10 +1,22 @@
 import { findById, calcOrderTotal } from '../common/utils.js';
 import shoes from '../data/shoes.js';
-import cart from '../data/cart.js';
 import renderTableRow from './renderLineItems.js';
 
 const tbody = document.querySelector('tbody');
 const orderTotalCell = document.getElementById('order-total-cell');
+const placeOrderButton = document.getElementById('place-order-button');
+
+const cartInLocalStorage = localStorage.getItem('CART');
+let cart;
+// check for local storage cart
+if (cartInLocalStorage) {
+    // string to parse
+    cart = JSON.parse(cartInLocalStorage);
+}
+else {
+    //initialize
+    cart = [];
+}
 
 for (let i = 0; i < cart.length; i++) {
     const cartItem = cart[i];
@@ -16,3 +28,17 @@ for (let i = 0; i < cart.length; i++) {
 
 const orderTotal = calcOrderTotal(cart, shoes);
 orderTotalCell.textContent = orderTotal;
+
+// add validator for cart
+if (cart.length === 0 ) {
+    placeOrderButton.disabled = true;
+}
+else {
+    // adds event listener if cart has objects
+    placeOrderButton.addEventListener('click', () => {
+        localStorage.removeItem('CART');
+        // true, 2 to make prettier
+        alert('Shoes ordered: ' + JSON.stringify(cart, true, 2));
+        window.location = '../';
+    });
+}
