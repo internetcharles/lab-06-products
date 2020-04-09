84c444573e3c2430d5abb1442c3b7e9b7e21572b
internetcharles <charlesodellsmith@gmail.com>
2020-04-09 09:31:17 -0700

    added store add button functionality

--- shoes/renderShoe.js
+++ shoes/renderShoe.js
@@ -1,3 +1,5 @@
+import { findById } from '../common/utils.js';
+
 function renderShoe(shoes) {
     const li = document.createElement('li');
     li.className = shoes.category;
@@ -27,6 +29,49 @@ function renderShoe(shoes) {
     button.textContent = 'Add';
     button.value = shoes.id;
     button.className = 'button';
+    button.addEventListener('click', () => {
+
+        // variable for cart in local storage
+        let cartInLocalStorage = localStorage.getItem('CART');
+        //variable for JS cart
+        let cart;
+        // checks for cart in local storage
+        if (cartInLocalStorage) {
+            //JSON turns string cart into an object
+            cart = JSON.parse(cartInLocalStorage);
+        }
+        else {
+            // if no cart, empty cart is made
+            cart = [];
+        }
+
+        // so now cart exists either as it was or newly created
+        let itemInCart = findById(cart, shoes.id);
+
+        if (!itemInCart) {
+            itemInCart = {
+                id: shoes.id,
+                quantity: 1
+            };
+
+            // push a itemInCart into the cart
+            cart.push(itemInCart);
+        }
+        else {
+            // or add to the quantity
+            itemInCart.quantity++;
+        }
+
+        // convert shopping cart to string
+        const stringCart = JSON.stringify(cart);
+        // add to local storage
+        localStorage.setItem('CART', stringCart);
+
+        // alert user of new addition
+        alert('1 ' + shoes.name + ' added to your cart');
+
+    });
+
     p2.appendChild(button);
 
     return li;
